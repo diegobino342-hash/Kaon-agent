de1fe13151d7863f6ed97959d4efa56ef7c46bff
diegobino342-hash <diegobino342@gmail.com>
2026-02-02 18:56:20 -0300

    Delete app directory

--- app/learning/memory.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def save_result(data):
-    conn = sqlite3.connect("app/storage/kaon.db")
-    c = conn.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS history (asset, action, result)")
-    c.execute("INSERT INTO history VALUES (?, ?, ?)", data)
-    conn.commit()
-    conn.close()
