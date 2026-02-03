c20edc9579a593839705e0ba493e810a73aa6d34
diegobino342-hash <diegobino342@gmail.com>
2026-02-03 14:54:40 -0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 WS_URL = "wss://ws-us2.pusher.com/app/43474559fc2d8059c93e?protocol=7&client=js&version=8.4.0"
 ORIGIN = "https://www.homebroker.com"
 
-TIMEFRAME = 300  # M5
-MIN_PROBABILITY = 0.8
+SYMBOL = "AAPL-OTC"
+TIMEFRAME_SECONDS = 300  # M5
